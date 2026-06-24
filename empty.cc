d05035ca7cbd796e4cfb6d9a3606ebcd2e763719
aaditya <arkya212@gmail.com>
2026-06-24 09:32:53 +0300

    A small include, that does nothing except making a library work

--- empty.cc
+++ empty.cc
@@ -2,6 +2,8 @@
 // This file ensures the module has at least one translation unit
 // and prevents CMake from erroring when examples are separate.
 
+#include "empty.h"
+
 namespace ns3 {
 static int lxechoserver_placeholder = 0;
 } // namespace ns3
